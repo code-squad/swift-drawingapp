02608e7010325261a8deea6885982fd85b10dde8
Kim Jung <godrm77@gmail.com>
2022-10-11 10:46:47 +0900

    프로젝트 초기 설정

---
+++ swift-drawingapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-drawingapp
+//
+//  Created by JK on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-drawingappUITests/swift_drawingappUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_drawingappUITestsLaunchTests.swift
+//  swift-drawingappUITests
+//
+//  Created by JK on 2022/07/04.
+//
+
+import XCTest
+
+class swift_drawingappUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
