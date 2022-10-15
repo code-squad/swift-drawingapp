19998f5deb5738f95c6aa49191ebe83207698aae
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-16 00:59:05 +0900

    추가: Canvas,Shape,StyleApplying 추가; 프로토콜을 따르는 Rect, Drawing 추가

---
+++ swift-drawingapp/Source/Shape.swift
@@ -0,0 +1,12 @@
+//
+//  Shape.swift
+//  swift-drawingapp
+//
+//  Created by Sunghyun Kim on 2022/10/16.
+//
+
+import Foundation
+
+protocol Shape: AnyObject {
+    var points: [Point] { get }
+}
---
+++ swift-drawingapp/Source/Structures/Size.swift
@@ -0,0 +1,13 @@
+//
+//  Size.swift
+//  swift-drawingapp
+//
+//  Created by Sunghyun Kim on 2022/10/16.
+//
+
+import Foundation
+
+struct Size {
+    var width: Double
+    var height: Double
+}
