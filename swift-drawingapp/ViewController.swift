bb33a32dc8345ca0c1ca9e521b9833692b1cd81d
JAEYOUNGYUN <lastmore9@gmail.com>
2022-10-15 14:16:24 +0900

    Add RectButton in ViewController

--- swift-drawingapp/ViewController.swift
+++ swift-drawingapp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    private let rectButton: RectButton = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setUI()
     }
 
+    private func setUI() {
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(rectButton)
 
+        NSLayoutConstraint.activate([
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
-
