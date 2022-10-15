d56125d3ff9db63838db99db81b54de010c9c553
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-16 01:14:53 +0900

    변경: canvas setter로 요구사항 변경

--- swift-drawingapp/Source/Structures/Size.swift
+++ swift-drawingapp/Source/Structures/Size.swift
@@ -10,4 +10,9 @@ import Foundation
 struct Size {
     var width: Double
     var height: Double
+    
+    func contains(_ point: Point) -> Bool {
+        0...width ~= point.x &&
+        0...height ~= point.y
+    }
 }
