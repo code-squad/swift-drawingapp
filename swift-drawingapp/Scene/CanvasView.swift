4187af64fe7a7137dcdc3255615d1301d56e2269
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-16 22:35:01 +0900

    추가: DrawingManagerViewModel 추가

--- swift-drawingapp/Scene/CanvasView.swift
+++ swift-drawingapp/Scene/CanvasView.swift
@@ -44,6 +44,10 @@ class CanvasView: UIView {
         
         viewModel.setSizeOfView(bounds.size)
         // FIXME: 업데이트가 되지 않는 문제 수정
+        updateView()
+    }
+    
+    func updateView() {
         updateShapeViews(viewModel.shapes)
     }
     
