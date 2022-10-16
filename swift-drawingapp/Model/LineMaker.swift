b621afb112860163cc15954f30139a101e3a1d77
sangjinKIM93 <larger611@gmail.com>
2022-10-16 14:44:06 +0900

    LineMaker 추가

---
+++ swift-drawingapp/Model/LineMaker.swift
@@ -0,0 +1,48 @@
+//
+//  LineMaker.swift
+//  swift-drawingapp
+//
+//  Created by 김상진 on 2022/10/16.
+//
+
+import Foundation
+import UIKit
+import QuartzCore
+
+class LineMaker {
+    
+    private var tempLayer = CAShapeLayer()
+    private var bezierPath = UIBezierPath()
+    
+    init() {}
+    
+    func getTempLayer() -> CAShapeLayer {
+        return tempLayer
+    }
+    
+    func getTempPath() -> UIBezierPath {
+        return bezierPath
+    }
+    
+    func resetTempData() {
+        tempLayer = CAShapeLayer()
+        bezierPath = UIBezierPath()
+    }
+    
+    func startLineDraw(point: CGPoint) -> CAShapeLayer {
+        resetTempData()
+        
+        bezierPath.move(to: point)
+        
+        tempLayer.lineWidth = 4
+        tempLayer.fillColor = UIColor.clear.cgColor
+        tempLayer.strokeColor = UIColor.blue.cgColor
+        
+        return tempLayer
+    }
+    
+    func addLinePath(to point: CGPoint) {
+        bezierPath.addLine(to: CGPoint(x: point.x, y: point.y))
+        tempLayer.path = bezierPath.cgPath
+    }
+}
