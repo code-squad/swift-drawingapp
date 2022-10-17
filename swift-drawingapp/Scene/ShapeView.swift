00d1fb3106219c231f506291c9eb85c5bc9503bb
Sunghyun Kim <sunghyun_k@icloud.com>
2022-10-18 00:50:08 +0900

    addSublayer 호출 위치 변경

--- swift-drawingapp/Scene/ShapeView.swift
+++ swift-drawingapp/Scene/ShapeView.swift
@@ -9,19 +9,20 @@ import UIKit
 
 class ShapeView: UIView {
     
-    private var shape = {
+    lazy private var shape = {
         let shape = CAShapeLayer()
         shape.lineJoin = .miter
         shape.lineWidth = 8
+        layer.addSublayer(shape)
         return shape
     }()
     
     func updatePath(_ viewModel: ShapeViewModel) {
+        shape.path = nil
         guard let path = makePath(from: viewModel.points) else { return }
         shape.path = path.cgPath
         shape.fillColor = viewModel.fillColor
         shape.strokeColor = viewModel.lineColor
-        layer.addSublayer(shape)
     }
     
     private func makePath(from points: [CGPoint]) -> UIBezierPath? {
