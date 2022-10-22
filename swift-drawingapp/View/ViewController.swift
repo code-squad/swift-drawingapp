3ff2e4b30fd10682ab8bb33bd4c9eddec8e4d33e
sangjinKIM93 <larger611@gmail.com>
2022-10-22 20:28:48 +0900

    SceneDelegate에서 VC실행

--- swift-drawingapp/View/ViewController.swift
+++ swift-drawingapp/View/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         viewModel.delegate = self
         
         setupView()
