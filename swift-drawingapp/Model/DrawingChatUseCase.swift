7699eeab982dbc026887017e0e97cfc94388113a
Sunghyun Kim <sunghyun_k@icloud.com>
2022-11-03 10:23:14 +0900

    유스케이스 분리

--- swift-drawingapp/Model/DrawingChatUseCase.swift
+++ swift-drawingapp/Model/DrawingChatUseCase.swift
@@ -1,5 +1,5 @@
 //
-//  DrawingChatService.swift
+//  DrawingChatUseCase.swift
 //  swift-drawingapp
 //
 //  Created by Sunghyun Kim on 2022/10/26.
@@ -9,9 +9,15 @@ import Foundation
 
 typealias ShapeData = [Point]
 
-protocol DrawingChatServiceProviding {
+protocol LoginUseCase {
     func login() async throws
+}
+
+protocol SendShapeUseCase {
     func sendShapes(_ shapes: [any ShapeProtocol]) async throws
+}
+
+protocol ReceiveShapeUseCase {
     var shapeStream: AnyAsyncSequence<[ShapeData]> { get }
 }
 
